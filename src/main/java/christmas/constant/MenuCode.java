fedc56a4312816801bb97dd09d172d9badc1eb35
Chenowoo <cjsgusdn14@naver.com>
2023-11-11 13:49:30 +0900

    feat(MenuCode): 메뉴 코드가 담긴 상수 추가

--- src/main/java/christmas/constant/MenuCode.java
+++ src/main/java/christmas/constant/MenuCode.java
@@ -1,4 +1,27 @@
 package christmas.constant;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum MenuCode {
+    APPETIZER("에피타이저", Arrays.asList(Menu.MUSHROOM_SOUP.getMenu(), Menu.TAPAS.getMenu(), Menu.CAESAR_SALAD.getMenu())),
+    MAIN("메인", Arrays.asList(Menu.T_BONE_STAEK.getMenu(), Menu.BARBECUE_LIB.getMenu(), Menu.SEAFOOD_PASTA.getMenu(), Menu.CHRISTMAS_PASTA.getMenu())),
+    DESSERT("디저트", Arrays.asList(Menu.CHOCOLATE_CAKE.getMenu(), Menu.ICE_CREAM.getMenu())),
+    BEVERAGE("음료", Arrays.asList(Menu.ZERO_COLA.getMenu(), Menu.RED_WINE.getMenu(), Menu.CHAMPAGNE.getMenu()));
+
+    private String menuCode;
+    private List<String> menu;
+
+    MenuCode(String menuCode, List<String> menu) {
+        this.menuCode = menuCode;
+        this.menu = menu;
+    }
+
+    public String getMenuCode() {
+        return menuCode;
+    }
+
+    public List<String> getMenu() {
+        return menu;
+    }
 }
