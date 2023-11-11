395a884b70c87a3ed3fdd387433df4cf1b8f9eaa
Chenowoo <cjsgusdn14@naver.com>
2023-11-11 13:49:12 +0900

    feat(Menu): 메뉴 정보가 담긴 상수 추가

--- src/main/java/christmas/constant/Menu.java
+++ src/main/java/christmas/constant/Menu.java
@@ -1,4 +1,32 @@
 package christmas.constant;
 
 public enum Menu {
+    MUSHROOM_SOUP("양송이수프", 6000),
+    TAPAS("타파스", 5500),
+    CAESAR_SALAD("시저샐러드", 8000),
+    T_BONE_STAEK("티본스테이크", 55000),
+    BARBECUE_LIB("바비큐립",54000),
+    SEAFOOD_PASTA("해산물파스타", 35000),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000),
+    CHOCOLATE_CAKE("초코케이크", 15000),
+    ICE_CREAM("아이스크림", 5000),
+    ZERO_COLA("제로콜라", 3000),
+    RED_WINE("레드와인", 60000),
+    CHAMPAGNE("샴페인", 25000);
+
+    private String menu;
+    private int price;
+
+    Menu(String menu, int price) {
+        this.menu = menu;
+        this.price = price;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
