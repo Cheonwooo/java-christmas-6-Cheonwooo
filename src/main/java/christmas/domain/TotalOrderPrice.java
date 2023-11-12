b53492ec12236ed6ac1acea758f48bfdc83b3b08
Chenowoo <cjsgusdn14@naver.com>
2023-11-13 02:05:57 +0900

    feat(TotalOrderPrice): 할인 전 총주문 금액을 구하는 기능 구현

---
+++ src/main/java/christmas/domain/TotalOrderPrice.java
@@ -0,0 +1,25 @@
+package christmas.domain;
+
+import christmas.constant.MenuBoard;
+
+import java.util.Map;
+
+public class TotalOrderPrice {
+
+    public int orderPrice(Map<String, Integer> orderMenu) {
+        int totalPrice = 0;
+
+        for(Map.Entry<String, Integer> entry : orderMenu.entrySet()) {
+            totalPrice += calculatePrice(entry.getKey(), entry.getValue());
+        }
+
+        return totalPrice;
+    }
+
+    public int calculatePrice(String menu, int menuCount) {
+        MenuBoard menuInfo = MenuBoard.valueOfMenu(menu);
+        int price = menuInfo.getPrice() * menuCount;
+
+        return price;
+    }
+}
