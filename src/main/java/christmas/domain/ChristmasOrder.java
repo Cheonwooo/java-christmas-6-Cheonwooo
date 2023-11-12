303eaa1a0ca5908abbd6cdb0f4cb78419e62df71
Chenowoo <cjsgusdn14@naver.com>
2023-11-13 00:57:26 +0900

    feat(ChristmasOrder): 메뉴 이름과 개수를 반환하는 기능 구현

--- src/main/java/christmas/domain/ChristmasOrder.java
+++ src/main/java/christmas/domain/ChristmasOrder.java
@@ -2,6 +2,10 @@ package christmas.domain;
 
 import christmas.view.InputView;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class ChristmasOrder {
     InputView inputView = new InputView();
 
@@ -11,4 +15,16 @@ public class ChristmasOrder {
         return date;
     }
 
+    public Map<String, Integer> getOrderMenu() {
+        String menu = inputView.getMenu();
+        Map<String, Integer> orderMenu = countingMenu(menu.split(","));
+
+        return orderMenu;
+    }
+
+    public Map<String, Integer> countingMenu(String[] input) {
+        return Arrays.stream(input)
+                .map(order -> order.split("-"))
+                .collect(Collectors.toMap(split -> split[0], split -> Integer.parseInt(split[1])));
+    }
 }
