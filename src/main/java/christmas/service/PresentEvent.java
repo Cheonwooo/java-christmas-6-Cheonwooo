6a3435384cc403227ad51ccf57ee4a9cf47d649a
Chenowoo <cjsgusdn14@naver.com>
2023-11-13 01:24:29 +0900

    feat(PresentEvent): 증정 이벤트 기능 구현

--- src/main/java/christmas/service/PresentEvent.java
+++ src/main/java/christmas/service/PresentEvent.java
@@ -1,4 +1,13 @@
 package christmas.service;
 
+import christmas.constant.MenuBoard;
+
 public class PresentEvent {
+
+    public String checkPresentEvent(int totalPrice) {
+        if(totalPrice >= 120000) {
+            return MenuBoard.CHAMPAGNE.getMenu();
+        }
+        return "없음";
+    }
 }
