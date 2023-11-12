05691979c3f661b6bae72b093a262fcd9def471a
Chenowoo <cjsgusdn14@naver.com>
2023-11-13 02:03:42 +0900

    feat(DiscountEvent): 할인 혜택별 가격을 반환하는 기능 구현

--- src/main/java/christmas/service/DiscountEvent.java
+++ src/main/java/christmas/service/DiscountEvent.java
@@ -1,4 +1,64 @@
 package christmas.service;
 
+import christmas.constant.*;
+
+import java.util.Map;
+
 public class DiscountEvent {
+
+    public int getDDayDiscount(int date) {
+        int dDayDiscount = 1000 + (date - 1) * 100;
+        if (date > 25) {
+            return 0;
+        }
+        return dDayDiscount;
+    }
+
+    public int getWeekdayDiscount(int dateNumber, Map<String, Integer> menu) {
+        DayDiscount dayDiscount = DayDiscount.findDiscountForDate(dateNumber);
+
+        if (dayDiscount.getWeek().equals("주말 할인")) {
+            return 0;
+        }
+        int weekdayDiscount = getDiscount(dayDiscount.getDiscountMenuCode(), menu);
+
+        return weekdayDiscount;
+    }
+
+    public int getWeekendDisCount(int dateNumber, Map<String, Integer> menu) {
+        DayDiscount dayDiscount = DayDiscount.findDiscountForDate(dateNumber);
+
+        if (dayDiscount.getWeek().equals("평일 할인")) {
+            return 0;
+        }
+        int weekendDiscount = getDiscount(dayDiscount.getDiscountMenuCode(), menu);
+
+        return weekendDiscount;
+    }
+
+    public int getDiscount(String discountMenuCode, Map<String, Integer> menu) {
+        int discountMenuCount = 0;
+        for (Map.Entry<String, Integer> entry : menu.entrySet()) {
+            String menuCode = MenuCode.findByMenuCode(entry.getKey()).getMenuCode();
+
+            if (discountMenuCode.equals(menuCode)) {
+                discountMenuCount += entry.getValue();
+            }
+        }
+        int menuDiscount = MenuDiscount.getMenuCategory(discountMenuCode).getDiscount() * discountMenuCount;
+
+        return menuDiscount;
+    }
+
+    public int getStarDiscount(int dateNumber) {
+        StarDiscount starDiscount = StarDiscount.findDiscountForStar(dateNumber);
+
+        if (starDiscount == null) {
+            return 0;
+        }
+
+        int discountForStar = starDiscount.getDiscount();
+
+        return discountForStar;
+    }
 }
