4f43c424d43cde2a37741f2bf6a9fc661651577f
Chenowoo <cjsgusdn14@naver.com>
2023-11-12 18:31:07 +0900

    rename(VisitDateValidationTest): 테스트 클래스명 변경

--- src/test/java/christmas/validation/VisitDateValidationTest.java
+++ src/test/java/christmas/validation/VisitDateValidationTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class InputValidationTest {
+class VisitDateValidationTest {
     private VisitDateValidation visitDateValidation;
 
     @BeforeEach
