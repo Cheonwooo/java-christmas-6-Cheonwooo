7d6c8d8602cfb051bd05b48d2859bf8eaab0f10d
Chenowoo <cjsgusdn14@naver.com>
2023-11-14 08:56:58 +0900

    feat(Application): 크리스마스 이벤트 예약 기능 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,13 @@
 package christmas;
 
+import christmas.domain.ChristmasOrder;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        ChristmasPromotion christmasPromotion = new ChristmasPromotion();
+
+        christmasPromotion.getReservation();
     }
 }
