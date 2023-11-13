58f9622f1d15cd50b137f36317e213c594fee73a
Chenowoo <cjsgusdn14@naver.com>
2023-11-13 15:43:28 +0900

    feat(Badge): 배지 이벤트 관련 enum 클래스 생성

---
+++ src/main/java/christmas/constant/Badge.java
@@ -0,0 +1,26 @@
+package christmas.constant;
+
+public enum Badge {
+
+    SANTA("산타", 20000),
+    TREE("트리", 10000),
+    STAR("별", 5000);
+
+    private final String badge;
+    private final int price;
+
+    Badge(String badge, int price) {
+        this.badge = badge;
+        this.price = price;
+    }
+
+    public String getBadge() {
+        return badge;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+
+}
