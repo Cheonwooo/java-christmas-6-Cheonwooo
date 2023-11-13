70378141962022cc96f10219c206e5abfadcface
Chenowoo <cjsgusdn14@naver.com>
2023-11-13 15:54:33 +0900

    feat(BadgeEvent): 할인 가격별 배지를 구하는 기능 구현

--- src/main/java/christmas/service/BadgeEvent.java
+++ src/main/java/christmas/service/BadgeEvent.java
@@ -1,4 +1,15 @@
 package christmas.service;
 
+import christmas.constant.Badge;
+
 public class BadgeEvent {
+
+    public static String getBadgeCategory(int totalDiscount) {
+        for(Badge badgeCategory : Badge.values()) {
+            if(totalDiscount >= badgeCategory.getPrice()) {
+                return badgeCategory.getBadge();
+            }
+        }
+        return "없음";
+    }
 }
