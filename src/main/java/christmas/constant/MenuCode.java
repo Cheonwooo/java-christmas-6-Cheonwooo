e71ebb204fbf4b6c00ea6ed85217812f767d4ca9
Chenowoo <cjsgusdn14@naver.com>
2023-11-13 10:28:23 +0900

    feat(MenuCode): 메뉴 이름을 입력하면 어떤 메뉴 카테고리에 속하는지 반환하는 기능 구현

--- src/main/java/christmas/constant/MenuCode.java
+++ src/main/java/christmas/constant/MenuCode.java
@@ -4,17 +4,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum MenuCode {
-    APPETIZER("에피타이저", Arrays.asList(Menu.MUSHROOM_SOUP.getMenu(), Menu.TAPAS.getMenu(), Menu.CAESAR_SALAD.getMenu())),
-    MAIN("메인", Arrays.asList(Menu.T_BONE_STAEK.getMenu(), Menu.BARBECUE_LIB.getMenu(), Menu.SEAFOOD_PASTA.getMenu(), Menu.CHRISTMAS_PASTA.getMenu())),
-    DESSERT("디저트", Arrays.asList(Menu.CHOCOLATE_CAKE.getMenu(), Menu.ICE_CREAM.getMenu())),
-    BEVERAGE("음료", Arrays.asList(Menu.ZERO_COLA.getMenu(), Menu.RED_WINE.getMenu(), Menu.CHAMPAGNE.getMenu()));
+    APPETIZER("에피타이저", Arrays.asList(MenuBoard.MUSHROOM_SOUP.getMenu(), MenuBoard.TAPAS.getMenu(), MenuBoard.CAESAR_SALAD.getMenu())),
+    MAIN("메인", Arrays.asList(MenuBoard.T_BONE_STEAK.getMenu(), MenuBoard.BARBECUE_LIB.getMenu(), MenuBoard.SEAFOOD_PASTA.getMenu(), MenuBoard.CHRISTMAS_PASTA.getMenu())),
+    DESSERT("디저트", Arrays.asList(MenuBoard.CHOCOLATE_CAKE.getMenu(), MenuBoard.ICE_CREAM.getMenu())),
+    BEVERAGE("음료", Arrays.asList(MenuBoard.ZERO_COLA.getMenu(), MenuBoard.RED_WINE.getMenu(), MenuBoard.CHAMPAGNE.getMenu()));
 
-    private String menuCode;
-    private List<String> menu;
+    private final String menuCode;
+    private final List<String> menuNames;
 
-    MenuCode(String menuCode, List<String> menu) {
+    MenuCode(String menuCode, List<String> menuNames) {
         this.menuCode = menuCode;
-        this.menu = menu;
+        this.menuNames = menuNames;
     }
 
     public String getMenuCode() {
@@ -22,6 +22,18 @@ public enum MenuCode {
     }
 
     public List<String> getMenu() {
-        return menu;
+        return menuNames;
+    }
+
+    public static MenuCode findByMenuCode(String menu) {
+        return Arrays.stream(MenuCode.values())
+                .filter(code -> code.hasMenuCode(menu))
+                .findAny()
+                .orElse(null);
+    }
+
+    public boolean hasMenuCode(String menu) {
+        return menuNames.stream()
+                .anyMatch(name -> name.equals(menu));
     }
 }
