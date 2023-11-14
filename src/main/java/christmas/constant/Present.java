6ca2d946c7fa35f854aafaed0f68c5fb637783fa
Chenowoo <cjsgusdn14@naver.com>
2023-11-14 18:57:51 +0900

    feat(Present): 총주문 금액이 10000원보다 작은 경우 이벤트 대상자 아닌 상수 추가

--- src/main/java/christmas/constant/Present.java
+++ src/main/java/christmas/constant/Present.java
@@ -2,7 +2,8 @@ package christmas.constant;
 
 public enum Present {
     CHAMPAGNE(MenuBoard.CHAMPAGNE.getMenu(), 120000),
-    NOTHING("없음", 10000);
+    NO_PRESENT("없음", 10000),
+    NOT_EVENT_TARGET("대상아님", 0);
 
     private final String present;
     private final int totalPrice;
