43f75b5fb7dff5266302b648e64d15ac78f1feef
Chenowoo <cjsgusdn14@naver.com>
2023-11-14 17:50:33 +0900

    feat(Badge): 총혜택 금액이 5000원보다 작은 경우 없음배지 추가

--- src/main/java/christmas/constant/Badge.java
+++ src/main/java/christmas/constant/Badge.java
@@ -4,7 +4,8 @@ public enum Badge {
 
     SANTA("산타", 20000),
     TREE("트리", 10000),
-    STAR("별", 5000);
+    STAR("별", 5000),
+    NOTHING("없음", 0);
 
     private final String badge;
     private final int price;
